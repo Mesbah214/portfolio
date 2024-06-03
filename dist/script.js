ee65be9cd736b0f477e25861a011a163dd6fe03e
Mesbah Uddin <msbh214@gmail.com>
2024-06-04 00:05:12 +0600

    Added prettier, formatted code

--- dist/script.js
+++ dist/script.js
@@ -1,16 +1,16 @@
 function submitForm() {
-  const form = document.getElementById("custom-form");
-  const success = document.getElementById("success");
+    const form = document.getElementById('custom-form')
+    const success = document.getElementById('success')
 
-  form.style.display = "none";
-  success.style.display = "block";
+    form.style.display = 'none'
+    success.style.display = 'block'
 
-  const formData = new FormData(form);
-  const url = "https://formsubmit.co/087f50430227d91717ae16a42b37c9f4";
-  fetch(url, {
-    method: "POST",
-    body: formData,
-  })
+    const formData = new FormData(form)
+    const url = 'https://formsubmit.co/087f50430227d91717ae16a42b37c9f4'
+    fetch(url, {
+        method: 'POST',
+        body: formData,
+    })
 
-  return false;
+    return false
 }
